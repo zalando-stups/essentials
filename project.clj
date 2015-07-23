720c793d4b56f1a3855fc9e3a0787919c721e216
Tobias Sarnowski <tobias.sarnowski@zalando.de>
2015-07-23 12:46:25 +0200

    Version 0.7.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.zalando.stups/essentials "0.6.0"
+(defproject org.zalando.stups/essentials "0.7.0-SNAPSHOT"
             :description "Microservice for resource types and scopes."
             :url "https://github.com/zalando-stups/essentials"
 
