a65771267804fb7d6f346c2626493f78822336ac
Andre Hartmann <andre.hartmann@zalando.de>
2015-09-15 13:15:01 +0200

    Version 0.11.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.zalando.stups/essentials "0.10.0"
+(defproject org.zalando.stups/essentials "0.11.0-SNAPSHOT"
             :description "Microservice for resource types and scopes."
             :url "https://github.com/zalando-stups/essentials"
 
