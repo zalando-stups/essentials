99dfc6eed7f9bb85f8801cee4bafa0361a53cd28
Henning Jacobs <henning@jacobs1.de>
2015-09-17 17:48:44 +0200

    Version 0.11.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.zalando.stups/essentials "0.11.0-SNAPSHOT"
+(defproject org.zalando.stups/essentials "0.11.0"
             :description "Microservice for resource types and scopes."
             :url "https://github.com/zalando-stups/essentials"
 
